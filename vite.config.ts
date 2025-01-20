e3ccb0ee114dcae4dfb69190a11240a6c3475eaa
Norman <85535885@qq.com>
2025-01-20 23:55:27 +0800

    设置部署目录为/logMonitorWeb/

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+//   部署目录为logMonitorWeb(norman.wang/logMonitorWeb/index.html)
+    base: '/logMonitorWeb/',
 })
